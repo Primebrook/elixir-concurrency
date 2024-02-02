1456ad5943041ddf9057e2fdecc1b3f6d83621df
Brook <71849503+Primebrook@users.noreply.github.com>
2024-02-02 16:25:47 +0000

    make reading the server state (`Calculator.value/1`) synchronous

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -3,41 +3,48 @@ defmodule Calculator do
   A simple calculator server that maintains a state - the current value/sum.
   """
 
-  # Client functions
+  ### Client functions ###
   def start do
     spawn(fn -> loop(0) end)
   end
 
-  def get_value do
+  # (synchronous message passing - i.e. send message then immediately wait for the response)
+  def value(server_pid) do
+    send(server_pid, {:value, self()})
+
     receive do
-      {:response, value} -> {:response, value}
-    after
-      5000 -> {:error, "timeout"}
+      {:response, value} -> {:ok, value}
     end
   end
 
-  def value(server_pid), do: send(server_pid, {:value, self()})
+  # (asynchronous arithmetic operations - i.e send message and DOESN'T wait for response)
   def add(server_pid, value), do: send(server_pid, {:add, value})
   def sub(server_pid, value), do: send(server_pid, {:sub, value})
   def mul(server_pid, value), do: send(server_pid, {:mul, value})
   def div(server_pid, value), do: send(server_pid, {:div, value})
 
-  # Server functions
+  ### Server functions ###
   defp loop(current_value) do
     new_value =
       receive do
-        {:value, caller_pid} ->
-          send(caller_pid, {:response, current_value})
-          current_value
-
-        {:add, number} -> current_value + number
-        {:sub, number} -> current_value - number
-        {:mul, number} -> current_value * number
-        {:div, number} -> current_value / number
-
-        invalid_request -> IO.puts("Invalid Request: #{inspect invalid_request}") ; current_value
+        msg -> process_message(current_value, msg)
       end
 
     loop(new_value)
+  end
+
+  defp process_message(current_value, {:value, caller_pid}) do
+    send(caller_pid, {:response, current_value})
+    current_value
+  end
+
+  defp process_message(current_value, {:add, number}), do: current_value + number
+  defp process_message(current_value, {:sub, number}), do: current_value - number
+  defp process_message(current_value, {:mul, number}), do: current_value * number
+  defp process_message(current_value, {:div, number}), do: current_value / number
+
+  defp process_message(current_value, invalid_request) do
+    IO.puts("Invalid Request: #{inspect(invalid_request)}")
+    current_value
   end
 end
