f5d6d6f70ca1b40d755be13627b7899d4392e31c
Brook <71849503+Primebrook@users.noreply.github.com>
2024-02-06 11:05:14 +0000

    update moduledoc

--- lib/gen_server/key_value_store.ex
+++ lib/gen_server/key_value_store.ex
@@ -1,12 +1,6 @@
 defmodule GenServer.KeyValueStore do
   @moduledoc """
-  The KeyValueStore callback module. This will be running in the server process.
-
-  Recall the callback process must implement: init/0 and handle_call/2 - these  are both callback functions
-  used internally by the generic server process code. 
-
-  In contrast, we implement the interface functions: start/0, get/2 and put/3 so that the client never has
-  to know about the ServerProcess module.
+  The KeyValueStore callback module.
   """
   use GenServer
 
